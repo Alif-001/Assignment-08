f93b0dabea580e8a830663e20670d2759b715dea
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2021-09-26 17:46:43 +0600

    api data added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,67 @@
 import React from "react";
+import "./Main.css";
 
 const Main = () => {
   return (
-    <div>
-      <h4>this is main part</h4>
+    <div className="main-container">
+      <div className="developers-section">
+        <h2>Developers</h2>
+        <div className="mx-5">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">This is a short card.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="total-section">
+        <h3>total</h3>
+        <p>this is total</p>
+      </div>
     </div>
   );
 };
